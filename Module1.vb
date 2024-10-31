cf1fe384a1eee39cf77447491074f909783b094f
salvadorjhai <jhai_salvador@yahoo.com>
2024-10-31 16:04:53 +0800

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,71 @@
+﻿Imports LLama.Common
+Imports LLama
+Imports System.IO
+Imports LLama.Sampling
+
+Module Module1
+
+    Sub Main()
+
+        Console.Title = "SIWSIWIT"
+
+        ' can be downloaded in https://huggingface.co/TheBloke/phi-2-GGUF
+        Dim modelPath = Path.Combine(Directory.GetCurrentDirectory(), "phi-2.Q8_0.gguf")
+
+        Dim parameters = New ModelParams(modelPath) With {
+            .ContextSize = 1024,
+            .GpuLayerCount = 10
+        }
+
+        Using model = LLamaWeights.LoadFromFile(parameters)
+            Using context = model.CreateContext(parameters)
+                Dim executor = New InteractiveExecutor(context)
+
+                Dim chatHistory = New ChatHistory()
+                chatHistory.AddMessage(AuthorRole.System, "Transcript of a dialog, where the User interacts with an Assistant named Birdie. Birdie is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately and with precision. Dont include 'User:' or 'Assistant:' on your response.")
+                chatHistory.AddMessage(AuthorRole.User, "Hello, Birdie.")
+                chatHistory.AddMessage(AuthorRole.Assistant, "Hello. How may I help you today?")
+
+                Dim session = New ChatSession(executor, chatHistory)
+                session.WithOutputTransform(New LLamaTransforms.KeywordTextOutputStreamTransform({"User:", "�", "Assistant:"}, 5))
+
+                Dim inferenceParams = New InferenceParams() With {
+                    .AntiPrompts = {"User:"}.ToList,
+                    .MaxTokens = 256,
+                    .SamplingPipeline = New DefaultSamplingPipeline() With {
+                        .Temperature = 0.9
+                    }
+                }
+
+                Console.Clear()
+                Console.ForegroundColor = ConsoleColor.Yellow
+                Console.WriteLine("> Hello World!")
+                Console.WriteLine("> How may I help you today?")
+                Console.ForegroundColor = ConsoleColor.White
+
+                Do While True
+                    Dim userInput = Console.ReadLine()
+                    If String.IsNullOrWhiteSpace(userInput) = False Then
+                        If userInput.ToLower = "stop" Then Exit Do
+
+                        Console.ForegroundColor = ConsoleColor.Yellow
+                        Console.WriteLine("generating response ... please wait ... ")
+
+                        Dim response As IAsyncEnumerable(Of String) = session.ChatAsync(New ChatHistory.Message(AuthorRole.User, userInput), inferenceParams)
+                        Dim str1 As New List(Of String)
+                        For Each r In response.ToListAsync().Result
+                            str1.Add(r)
+                        Next
+
+                        Console.ForegroundColor = ConsoleColor.Yellow
+                        Console.WriteLine(String.Join("", str1).Trim)
+                        Console.ForegroundColor = ConsoleColor.White
+                    End If
+                Loop
+
+            End Using
+        End Using
+
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,9 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("32ff8ec3-628b-49fb-a545-3f555b98b962")>
